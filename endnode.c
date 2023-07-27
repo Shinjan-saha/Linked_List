ac7f63c56a35e6cbbe012970e0b926c15fc139c9
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-07-27 20:28:43 +0530

    Add files via upload

---
+++ endnode.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+void add_at_end(struct node *head, int data) {
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = (struct node*)malloc(sizeof(struct node));
+
+    temp->data = data;
+    temp->link = NULL;
+
+    while (ptr->link != NULL) {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+}
+
+int main() {
+
+    struct node *head = NULL;
+
+
+    add_at_end(head, 67);
+
+    return 0;
+}
